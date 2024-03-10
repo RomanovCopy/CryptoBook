c067df0ed475058f79dd65cd60dac3985e548d3d
RomanovCopy <RomanovCopy@gmail.com>
2024-03-10 20:04:47 +0300

    fix10032004

---
+++ CryptoBook/Interfaces/IPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CryptoBook.Interfaces
+{
+    interface IPage
+    {
+        ICommand Loaded { get; }
+        ICommand Closing { get; }
+        ICommand Closed { get; }
+    }
+}
