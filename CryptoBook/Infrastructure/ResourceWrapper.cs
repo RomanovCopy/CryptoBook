65dd995aa291535c5bbe1c1bceab1acf76850eee
AsusVivobook <RomanovCopy@gmail.com>
2025-05-24 21:45:47 +0300

    2405252145

--- CryptoBook/Infrastructure/ResourceWrapper.cs
+++ CryptoBook/Infrastructure/ResourceWrapper.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace CryptoBook.Infrastructure
 {
-    public class ResourceWrapper: INotifyPropertyChanged
+    public class ResourceWrapper: ViewModelBase
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
+        //public event PropertyChangedEventHandler? PropertyChanged;
 
         public string? this[string key] => Properties.Resources.ResourceManager.GetString(key);
 
-        public void OnCultureChanged()
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
-        }
+        //public void OnCultureChanged()
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+        //}
     }
 
 }
