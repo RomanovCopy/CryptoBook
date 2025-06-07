2115e1288339d2387f8469340f2a308aac741bca
AsusVivobook <RomanovCopy@gmail.com>
2025-06-07 18:12:24 +0300

    0706251812

---
+++ CryptoBook/Markup/ResolveConverterExtension.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CryptoBook.Markup
+{
+    public class ResolveConverterExtension: MarkupExtension
+    {
+        private static readonly ConcurrentDictionary<(Type, string), IValueConverter> converterCashe = new();
+
+        public Type ConverterType { get; set; }
+        public string Parametr { get; set; }
+
+        public ResolveConverterExtension(ILifetimeScope scope)
+        {
+        }
+
+        public override IValueConverter ProvideValue(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
