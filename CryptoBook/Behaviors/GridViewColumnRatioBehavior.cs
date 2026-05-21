a4135e6817713497e24ffdd1dd4bf05b7ddf65c0
RomanovCopy <RomanovCopy@gmail.com>
2026-05-21 13:37:43 +0300

    2105261337

--- CryptoBook/Behaviors/GridViewColumnRatioBehavior.cs
+++ CryptoBook/Behaviors/GridViewColumnRatioBehavior.cs
@@ -105,7 +105,6 @@ namespace CryptoBook.Behaviors
             // drag-resize заголовков колонок
             AssociatedObject.AddHandler(FrameworkElement.SizeChangedEvent, new SizeChangedEventHandler(OnAnySizeChanged),
             handledEventsToo: true);
-            AssociatedObject.AddHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(OnHeaderClick));
 
             //по окончании изменения колонки
             AssociatedObject.AddHandler(Thumb.DragCompletedEvent, new DragCompletedEventHandler(OnThumbDragCompleted), true);
@@ -119,47 +118,6 @@ namespace CryptoBook.Behaviors
 
         }
 
-        private void OnHeaderClick(object sender, RoutedEventArgs e)
-        {
-            if(e.OriginalSource is GridViewColumnHeader header && header.Tag is string name)
-            {
-                if(sender is System.Windows.Controls.ListView lv)
-                {
-                    var view = CollectionViewSource.GetDefaultView(lv.ItemsSource);
-                    view.SortDescriptions.Clear();
-                    switch(name)
-                    {
-                        case "Name":
-                        {
-                            view.SortDescriptions.Add(new SortDescription("Kind", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add( new SortDescription("Name", ListSortDirection.Ascending));
-                            break;
-                        }
-                        case "LastWriteTimeUtc":
-                        {
-                            view.SortDescriptions.Add(new SortDescription("Kind", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add(new SortDescription("LastWriteTimeUtc", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
-                            break;
-                        }
-                        case "Extension":
-                        {
-                            view.SortDescriptions.Add(new SortDescription("Kind", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add(new SortDescription("Extension", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
-                            break;
-                        }
-                        case "Size":
-                        {
-                            view.SortDescriptions.Add(new SortDescription("Kind", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add(new SortDescription("Size", ListSortDirection.Ascending));
-                            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
-                            break;
-                        }
-                    }
-                }
-            }
-        }
 
 
 
@@ -186,7 +144,6 @@ namespace CryptoBook.Behaviors
             if(ReflowOnSizeChanged)
                 AssociatedObject.SizeChanged -= OnListViewSizeChanged;
 
-            AssociatedObject.RemoveHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(OnHeaderClick));
 
             base.OnDetaching();
         }
