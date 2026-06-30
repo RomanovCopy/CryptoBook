5445b1d96491bd5510a740cca80d2e85ff8de434
msi_work <RomanovCopy@gmail.com>
2026-06-30 15:10:20 +0300

    3006261508

---
+++ CryptoBook/Interfaces/IKeyInputViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoBook.Interfaces
+{
+    public interface IKeyInputViewModel: IViewModel,IWindowOptions, IWindowWithId
+    {
+        string Title { get; }
+        string Message { get; }
+        bool ShowRepeatPassword { get; init; }
+
+    }
+}
---
+++ CryptoBook/ViewModels/KeyInputViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CryptoBook.Infrastructure;
+using CryptoBook.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CryptoBook.ViewModels
+{
+    public sealed class KeyInputViewModel :ViewModelBase, IKeyInputViewModel
+    {
+
+        public double WindowWidth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double WindowHeight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double WindowTop { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double WindowLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public WindowState WindowState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Guid WindowId => throw new NotImplementedException();
+
+        public KeyInputViewModel()
+        {
+            
+        }
+
+
+        public string Title { get; } = "Ключ шифрования";
+        public string Message { get; } = "Введите ключ шифрования:";
+
+        public bool ShowRepeatPassword { get; init; } = true;
+
+        public ICommand Loaded => throw new NotImplementedException();
+
+        public ICommand Close => throw new NotImplementedException();
+
+        public ICommand Closing => throw new NotImplementedException();
+
+        public ICommand Closed => throw new NotImplementedException();
+
+
+    }
+}
