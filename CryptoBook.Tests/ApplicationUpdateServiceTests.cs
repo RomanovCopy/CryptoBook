74ea3f4a607174ce23cdc500107bc89438aff24f
RomanovCopy <RomanovCopy@gmail.com>
2026-08-08 09:11:21 +0300

    Подготовлен релиз 1.1.0.2

--- CryptoBook.Tests/ApplicationUpdateServiceTests.cs
+++ CryptoBook.Tests/ApplicationUpdateServiceTests.cs
@@ -13,7 +13,7 @@ public sealed class ApplicationUpdateServiceTests
     {
         var provider = new AssemblyApplicationVersionProvider();
 
-        Assert.Equal("1.1.0.1", provider.GetCurrentVersion().ToString());
+        Assert.Equal("1.1.0.2", provider.GetCurrentVersion().ToString());
     }
 
     [Fact]
