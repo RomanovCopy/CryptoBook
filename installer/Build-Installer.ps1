2929e26c73c67a8ac62471a0b94402cd1bcc6d6d
msi_work <RomanovCopy@gmail.com>
2026-08-19 10:10:52 +0300

    Release 1.1.1.9

--- installer/Build-Installer.ps1
+++ installer/Build-Installer.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+(\.\d+)?([-.][0-9A-Za-z.-]+)?$')]
-    [string] $Version = '1.1.1.8',
+    [string] $Version = '1.1.1.9',
 
     [ValidateSet('Debug', 'Release')]
     [string] $Configuration = 'Release',
